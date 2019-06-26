d31eeffefd3b551e3cf866d1c00ac9b08f55a651
EmanoelGarcia <52226925+EmanoelGarcia@users.noreply.github.com>
2019-06-26 00:04:43 -0300

    Update Trabson.py

--- Trabson.py
+++ Trabson.py
@@ -1,5 +1,5 @@
 import cv2
-img = cv2.imread("teste_color.jpg"   , cv2.IMREAD_COLOR)
+img = cv2.imread("teste_color.jpg",cv2.IMREAD_COLOR)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 enhance = cv2.detailEnhance(img)
 cv2.imshow('teste', enhance)
