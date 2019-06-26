e4b8e2808272c8c9e91650dc6713e0d7ca1a9c54
EmanoelGarcia <52226925+EmanoelGarcia@users.noreply.github.com>
2019-06-25 23:52:31 -0300

    Add files via upload

---
+++ Trabson.py
@@ -0,0 +1,8 @@
+import cv2
+img = cv2.imread("teste_color.jpg"   , cv2.IMREAD_COLOR)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+enhance = cv2.detailEnhance(img)
+cv2.imshow('teste', enhance)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite("teste_result.jpg", img)
